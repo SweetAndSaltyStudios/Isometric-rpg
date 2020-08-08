8f11138783ce1071c12b3292afc20a287b976117
Mathias Mustonen <mathias.mustonen@gmail.com>
2020-08-08 21:55:02 +0300

    Project cleaning

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace SweetAndSaltyStudios
+{
+	public class Player : MonoBehaviour
+	{
+        #region VARIABLES
+
+        private Rigidbody2D rb2D;
+
+        #endregion VARIABLES
+
+        #region PROPERTIES
+
+        #endregion PROPERTIES
+
+        #region UNITY_FUNCTIONS
+
+        private void Awake()
+        {
+            rb2D = GetComponent<Rigidbody2D>();
+        }
+
+        #endregion UNITY_FUNCTIONS
+
+        #region CUSTOM_FUNCTIONS
+
+       
+
+        #endregion CUSTOM_FUNCTIONS
+    }
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace SweetAndSaltyStudios
+{
+	public class Tile : TileBase
+	{
+    #region VARIABLES
+        public int Foo;
+    #endregion VARIABLES
+
+    #region PROPERTIES
+	
+    #endregion PROPERTIES
+
+    #region UNITY_FUNCTIONS
+	
+    #endregion UNITY_FUNCTIONS
+
+    #region CUSTOM_FUNCTIONS
+	
+    #endregion CUSTOM_FUNCTIONS
+	}
+}
